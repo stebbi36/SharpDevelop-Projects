5ded913b66600f844b302b1d19e8259b47db94ea
stefanajons <stefanajons@gmail.com>
2015-01-05 02:09:37 +0000

    sharpdevelop

---
+++ sharpdevelop/sharpdevelop/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*inurl:adminlogin.asp
+ * 
+ * Created by SharpDevelop.
+ * User: stefán
+ * Date: 9.7.2014
+ * Time: 04:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace sharpdevelop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
